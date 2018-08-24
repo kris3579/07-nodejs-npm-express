2fb1c203b4cffeb4d763e09c8aa59b558da309a4
Kris3579 <kristianesvelt@hotmail.com>
2018-08-23 17:33:52 -0700

    fix

--- kris-candice/server.js
+++ kris-candice/server.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 const app = express();
+
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
@@ -15,14 +17,14 @@ app.post('/articles', (request, response) => {
   response.status(201).json(request.body);
 });
 
+app.get('/new', (request, response) => {
+  response.sendFile(path.join(__dirname + '/public/new.html'));
+});
+
 app.use(function (req, res) {
   res.status(404).send('sorry can\'t find that!');
 });
 
-app.get('/public/new.html', (req, res) => {
-  res.send('you found it!');
-});
-
-app.listen(PORT, function () {
+app.listen(PORT, function ()  {
   console.log('heard PORT 3000');
 });
